682758d45694c84b02a3eaa0654d8bd4761d1df0
Miguel Tang <A01610836@itesm.mx>
2022-02-22 23:49:35 -0600

    Agregar Lab6 Verificar Passwords

--- A01610836_Lab6_ValidarPasswords.js
+++ A01610836_Lab6_ValidarPasswords.js
@@ -1,4 +1,45 @@
-let input_contrasenia = document.getElementById("contrasenia");
+let input_password = document.getElementById("password");
 
-let boton_verificar = document.getElementById("verificar");
+function verificarPassword(contrasenia) {
+    let passwordString = contrasenia.toString();
+    let length = contrasenia.length;
+    let passwordUppercaseChar = passwordString.replace(/[^A-Z]/g,"");
+    let passwordLowercaseChar = passwordString.replace(/[^a-z]/g,"");
+    let passwordNumbers = passwordString.replace(/[^0-9]/g,"");
+    let passwordSymbols = passwordString.replace(/[^`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/g,"");
 
+    let casosVerificacion = "";
+    let flag = true;
+
+    if (passwordString.length < 8){
+        casosVerificacion += "-La contraseña tiene menos de 8 caracteres <br>";
+        flag = false;
+    }
+    if (passwordUppercaseChar.length < 1) {
+        casosVerificacion += "-La contraseña no tiene letras mayúsculas <br>";
+        flag = false;
+    }
+    if (passwordLowercaseChar.length < 1) {
+        casosVerificacion += "-La contraseña no tiene letras minúsculas <br>";
+        flag = false;
+    }
+    if (passwordNumbers.length < 1){
+        casosVerificacion += "-La contraseña no tiene numeros <br>";
+        flag = false;
+    }
+    if (passwordSymbols.length < 1){
+        casosVerificacion += "-La contraseña no tiene símbolos <br>";
+        flag = false;
+    } 
+    if (flag == true){
+        casosVerificacion += "La contraseña es segura";
+    }
+
+    return casosVerificacion;
+}
+
+boton_verificar.onclick = () => {
+    let boton_verificar = document.getElementById("verificacion");
+    boton_verificar.innerHTML = verificarPassword(input_password.value);
+    
+}
