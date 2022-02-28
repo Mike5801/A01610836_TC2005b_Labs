0dff3a59c7e0cf768b4d2ef4d18765ac40f68d4d
Miguel Tang <A01610836@itesm.mx>
2022-02-28 13:09:03 -0600

    Agregar Laboratorio 9 e iniciar problema3 laboratorio 8

--- A01610836_Lab8_BackEnd.js
+++ A01610836_Lab8_BackEnd.js
@@ -15,4 +15,49 @@ function promedioArreglo(arreglo){
 console.log(promedioArreglo(arreglo));
 
 
+//Problema dos
+
+const filesystem = require('fs');
+
+let string = "Hola mundo";
+
+function escribirString(string){
+    filesystem.writeFileSync('Lab8Problema2.txt', string);
+}
+
+escribirString(string);
+
+//Problema tres
+
+/*
+During the break the schoolchildren, boys and girls, formed a queue of n people in the canteen. Initially the children stood in the order they entered the canteen. However, after a while the boys started feeling awkward for standing in front of the girls in the queue and they started letting the girls move forward each second.
+
+Let's describe the process more precisely. Let's say that the positions in the queue are sequentially numbered by integers from 1 to n, at that the person in the position number 1 is served first. Then, if at time x a boy stands on the i-th position and a girl stands on the (i + 1)-th position, then at time x + 1 the i-th position will have a girl and the (i + 1)-th position will have a boy. The time is given in seconds.
+
+You've got the initial position of the children, at the initial moment of time. Determine the way the queue is going to look after t seconds.
+
+Input
+The first line contains two integers n and t (1 ≤ n, t ≤ 50), which represent the number of children in the queue and the time after which the queue will transform into the arrangement you need to find.
+
+The next line contains string s, which represents the schoolchildren's initial arrangement. If the i-th position in the queue contains a boy, then the i-th character of string s equals "B", otherwise the i-th character equals "G".
+
+Output
+Print string a, which describes the arrangement after t seconds. If the i-th position has a boy after the needed time, then the i-th character a must equal "B", otherwise it must equal "G".
+*/
+
+let n = console.promt("input n:");
+let t = console.promt("input t:");
+
+let string = console.prompt("string of B and G:");
+
+let stringArray = string.split('');
+
+for (let i = 0; i < stringArray.length; i++){
+    if (stringArray[i] == 'G'){
+    }
+    if (stringArray[i] == 'B'){
+        stringArray[i] = 'G';
+    }
+}
+
 
