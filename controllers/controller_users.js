4f56a05743f726d2c0e9492a4322ccb3e975ef58
Miguel Tang <A01610836@itesm.mx>
2022-03-14 11:01:22 -0600

    Agregar registro de usuarios

--- controllers/controller_users.js
+++ controllers/controller_users.js
@@ -11,10 +11,25 @@ exports.get_login =  (request, response, next) => {
 exports.login =  (request, response, next) => {
     if (User.login(request.body.nombre, request.body.passwd)) {
         request.session.usuario = request.body.nombre;
-        response.redirect('/modulo4/');
+        response.redirect('/modulo3/');
     } else {
         response.redirect('/users/login');
     }
+};
+
+exports.get_signup =  (request, response, next) => {
+    response.render('signup', {
+        usuario: request.session.usuario ? request.session.usuario : '',
+    });
+};
+
+exports.post_signup =  (request, response, next) => {
+    const nuevo_usuario = new User(request.body.username, request.body.password, request.body.nombre);
+    nuevo_usuario.save().then(() => {
+        response.redirect('/users/login');
+    }).catch((err) => {
+        console.log(err);
+    });
 };
 
 exports.logout =  (request, response, next) => {
